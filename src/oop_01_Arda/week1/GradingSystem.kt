10d97e55f57840582a9f2bd2a73208292444c695
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-04 16:12:23 +0700

    week01: implement grading logic using when expression

--- src/oop_01_Arda/week1/GradingSystem.kt
+++ src/oop_01_Arda/week1/GradingSystem.kt
@@ -4,4 +4,15 @@ fun main() {
     val name = "John Thor"
     val score = 80
 
+// Concatenation gaya lama
+    println("Nama: $name, Nilai: $score")
+
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else            -> "D"
+    }
+    println("Grade kamu: $grade")
+
 }
