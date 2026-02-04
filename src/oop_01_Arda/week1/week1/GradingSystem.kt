66e2bb12d37a68923241b82e4b07b2b2fe84ba0a
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-04 15:33:53 +0700

    week01: initial setup and procedural code

---
+++ src/oop_01_Arda/week1/week1/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_01_Arda.week1.week1
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+// Concatenation gaya lama
+    println("Nama: " + name + " Nilai: " + score)
+}
