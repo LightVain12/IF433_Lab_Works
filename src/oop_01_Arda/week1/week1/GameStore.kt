2abbc99083901d74eb67aab8495898e776c714d6
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-08 23:30:46 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_01_Arda/week1/week1/GameStore.kt
+++ src/oop_01_Arda/week1/week1/GameStore.kt
@@ -4,3 +4,6 @@ fun main() {
     val gameTitle = "GTA VI"
     val price = 750_000
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) (price * 20) / 100 else (price * 10) / 100
