f571cbe33d6cb14afc8252e7fdfece693f4e7981
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-08 23:24:51 +0700

    week01: (tugas 1) refactor variables to val and use string templates

--- src/oop_01_Arda/week1/week1/CircleCalculator.kt
+++ src/oop_01_Arda/week1/week1/CircleCalculator.kt
@@ -1,14 +1,20 @@
 package oop_01_Arda.week1.week1
 
-fun main() {
+fun main(args: Array<String>) {
     val radius = 7.0
     val pi = 3.14
 
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
-    println(checkSize(area))
+
+    checkSize(area)
 }
 
-fun checkSize(area: Double) =
-    if (area > 100) "This is a Big Circle" else "This is a Small Circle"
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
+}
---
+++ src/oop_01_Arda/week1/week1/GameStore.kt
@@ -0,0 +1,6 @@
+package oop_01_Arda.week1.week1
+
+fun main() {
+    val gameTitle = "GTA VI"
+    val price = 750_000
+}
