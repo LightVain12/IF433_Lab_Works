537a45421df6a557993a0883533af2a7c3f22185
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-08 23:29:19 +0700

    week01: (tugas 2) setup GameStore file

--- src/oop_01_Arda/week1/week1/GameStore.kt
+++ src/oop_01_Arda/week1/week1/GameStore.kt
@@ -3,4 +3,22 @@ package oop_01_Arda.week1.week1
 fun main() {
     val gameTitle = "GTA VI"
     val price = 750_000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    printReceipt(
+        title = gameTitle,
+        originalPrice = price,
+        finalPrice = finalPrice
+    )
+}
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) (price * 20) / 100 else (price * 10) / 100
+
+fun printReceipt(title: String, originalPrice: Int, finalPrice: Int) {
+    println("Judul      : $title")
+    println("Harga Asli : Rp $originalPrice")
+    println("Harga Akhir: Rp $finalPrice")
 }
