633d25a02490ff65924c44c14e4824815b25b7ab
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-17 13:31:29 +0700

    week02: add warning validation in init block

--- src/oop_02_Arda/Week2/week2/Student.kt
+++ src/oop_02_Arda/Week2/week2/Student.kt
@@ -4,4 +4,16 @@ class Student(
     val name: String,
     val nim: String,
     var major: String
-)
+) {
+    init {
+        if (nim.length != 11) {
+            println("WARNING: NIM harus 11 digit! Data yang dimasukkan tidak valid.")
+        }
+        if (name.isBlank()) {
+            println("WARNING: Nama tidak boleh kosong!")
+        }
+        if (major.isBlank()) {
+            println("WARNING: Jurusan tidak boleh kosong!")
+        }
+    }
+}
