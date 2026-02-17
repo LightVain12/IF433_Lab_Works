2c06285024a6a48453456577d295bd67811b2abe
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-17 13:19:12 +0700

    week02 : create basic Student class structure

--- src/oop_02_Arda/Week2/M2ClassF_Theory.kt
+++ src/oop_02_Arda/Week2/M2ClassF_Theory.kt
@@ -1,35 +1,8 @@
 package oop_02_Arda.Week2
 
-class Pemain {
-    var nama: String = "Tidak Terdaftar"
-    var speed: Int = 9
-    val shoot: Int = 8
-
-
-    init {
-        nama = "messi";
-        print("ini adalah constructor");
-    }
-
-    constructor(alias:String="Budi", kecepatan: Int=2) {
-        nama = alias;
-        speed = kecepatan;
-        println("ini adalah contstrutor2");
-    }
-
-    fun bawa_bola() {
-        println("si $nama sedang bawa bola")
-    }
-}
-
-fun main() {
-    val player = Pemain()
-    player.bawa_bola()
-    println("nama pemain: ${player.nama}")
-    println("speed: ${player.speed}")
-
-    val player2 = Pemain(alias = "Ronaldo", kecepatan = 99)
-    println("pemain popular: ${player2.nama}")
-    println("kecepatan: ${player2.speed}")
-}
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String
+)
 
