bad427c5e7c54d405d9db221595241dc503dea1c
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-24 21:54:08 +0700

    week03: fix setter using backing field

--- src/oop_03_Arda/Week3/week3/Employee.kt
+++ src/oop_03_Arda/Week3/week3/Employee.kt
@@ -3,8 +3,11 @@ package oop_03_Arda.Week3.week3
 class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
-            println("Mencoba set gaji ke: $value")
-            // PERINGATAN: Memanggil setter di dalam setter (Recursive)
-            this.salary = value
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0 // Gunakan field, bukan this.salary
+            } else {
+                field = value // Gunakan field untuk assign nilai asli
+            }
         }
 }
--- src/oop_03_Arda/Week3/week3/Main.kt
+++ src/oop_03_Arda/Week3/week3/Main.kt
@@ -1,2 +1,7 @@
 package oop_03_Arda.Week3.week3
 
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 // Ini akan meledakkan program Anda
+}
