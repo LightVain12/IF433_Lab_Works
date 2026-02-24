e8203f3a21fcf46c1929772a17d751aad4f2fa78
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-02-24 21:57:36 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_03_Arda/Week3/week3/Weapon.kt
@@ -0,0 +1,21 @@
+package oop_03_Arda.Week3.week3
+
+class Weapon(val name: String) {
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Peringatan: Damage tidak boleh negatif!")
+            } else if (value > 1000) {
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
