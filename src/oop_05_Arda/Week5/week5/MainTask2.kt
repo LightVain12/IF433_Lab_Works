4ef29a4720f8f4816c7a3571f9b8a5a10054c582
Arda Erdika Nanda <erdikaarda@gmail.com>
2026-03-04 08:48:40 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_05_Arda/Week5/week5/MainTask2.kt
+++ src/oop_05_Arda/Week5/week5/MainTask2.kt
@@ -1,28 +1,16 @@
 package oop_05_Arda.Week5.week5
 
 fun main() {
+    // 1. Buat objek EWallet dan CreditCard
     val eWallet = EWallet(accountName = "Arda EWallet", balance = 50000.0)
-    val creditCard = CreditCard(accountName = "Arda Credit Card", limit = 100000.0)
+    val creditCard = CreditCard(accountName = "Arda CreditCard", limit = 100000.0)
 
+    // 2. Masukkan ke dalam list bertipe Parent (PaymentMethod)
     val daftarPembayaran: List<PaymentMethod> = listOf(eWallet, creditCard)
-    val transactionAmount = 75000.0
 
-    println("=== PROSES TRANSAKSI ===")
+    println("=== PROSES TRANSAKSI AWAL ===")
+    // 3. Lakukan perulangan
     for (payment in daftarPembayaran) {
-        // Coba proses pembayaran
-        payment.processPayment(transactionAmount)
-
-        // Smart Casting Challenge (Checkpoint 11)
-        if (payment is EWallet) {
-            // Karena EWallet saldo awalnya 50000, transaksi 75000 pasti gagal.
-            // Kita deteksi menggunakan `is` lalu jalankan topUp untuk menutupi kekurangannya.
-            println("--> Mendeteksi EWallet gagal. Melakukan Top Up Otomatis...")
-            payment.topUp(50000.0)
-
-            // Coba transaksi lagi setelah top up
-            println("--> Mencoba transaksi kembali...")
-            payment.processPayment(transactionAmount)
-        }
-        println()
+        payment.processPayment(75000.0)
     }
 }
