b16e8c28e1bb5de6adaa1fe4c0d58c1831bca4c5
LightVain12 <erdikaarda@gmail.com>
2026-03-11 14:26:03 +0700

    week06: create Button class implementing Clickable

--- src/oop_06_Arda/Week6/week6/Clickable.kt
+++ src/oop_06_Arda/Week6/week6/Clickable.kt
@@ -1,9 +1,7 @@
 package oop_06_Arda.Week6.week6
 
-interface Clickable {
-    // ERROR: Property in an interface cannot have a backing field
-    val name: String = "Tombol Rahasia"
-
-    // Function without body (Implicitly Abstract)
-    fun click()
+class Button(override val name: String) : Clickable {
+    override fun click() {
+        println("Tombol '$name' berhasil diklik!")
+    }
 }
