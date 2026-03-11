338ec32642e3a28bb9054254225a37d981e58046
LightVain12 <erdikaarda@gmail.com>
2026-03-11 15:16:53 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_06_Arda/Week6/week6/MainTask.kt
@@ -0,0 +1,9 @@
+package oop_06_Arda.Week6.week6
+
+fun main() {
+    val lamp = SmartLamp(id = "L01", name = "Ruang Tamu")
+    val speaker = SmartSpeaker(id = "S01", name = "Google Nest Dapur")
+    val cctv = SmartCCTV(id = "C01", name = "Ezviz Garasi")
+
+    // (Hub belum dijalankan di checkpoint ini)
+}
