4d1cfe48f74b164ddba032e9395faaf70327af27
LightVain12 <erdikaarda@gmail.com>
2026-03-11 14:37:41 +0700

    week06: (trial) create Clickable interface with backing field error

--- src/oop_06_Arda/Week6/week6/Capabilities.kt
+++
@@ -1,9 +0,0 @@
-package oop_06_Arda.Week6.week6
-
-interface BluetoothConnectable {
-    fun connectToBluetooth()
-}
-
-interface Rechargeable {
-    fun chargeBattery()
-}
--- src/oop_06_Arda/Week6/week6/Watch.kt
+++
@@ -1,5 +0,0 @@
-package oop_06_Arda.Week6.week6
-
-abstract class Watch {
-    abstract fun showTime()
-}
