7a6ebc840cd1acae9c8c62398eccb166c70e759b
LightVain12 <erdikaarda@gmail.com>
2026-03-11 15:13:10 +0700

    week06: implement decoupled processCheckout in main

--- src/oop_06_Arda/Week6/week6/Clickable.kt
+++ src/oop_06_Arda/Week6/week6/Clickable.kt
@@ -1,5 +1,11 @@
 package oop_06_Arda.Week6.week6
 
+interface Clickable {
+    val name: String
+    fun click()
+}
+
+// Jika class Button ada di sini juga tidak apa-apa
 class Button(override val name: String) : Clickable {
     override fun click() {
         println("Tombol '$name' berhasil diklik!")
