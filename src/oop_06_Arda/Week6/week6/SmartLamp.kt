8369200f57684c7325636d42b3d775564bcdf407
LightVain12 <erdikaarda@gmail.com>
2026-03-11 15:14:37 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_06_Arda/Week6/week6/SmartLamp.kt
@@ -0,0 +1,11 @@
+package oop_06_Arda.Week6.week6
+
+class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Lampu '$name' dinyalakan.")
+    }
+
+    override fun turnOff() {
+        println("Lampu '$name' dimatikan.")
+    }
+}
