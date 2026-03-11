6e2fb525434d90efad86df0ed063dc4a63e94e3f
LightVain12 <erdikaarda@gmail.com>
2026-03-11 14:39:47 +0700

    week06: create Button class implementing Clickable

--- src/oop_06_Arda/Week6/week6/Clickable.kt
+++ src/oop_06_Arda/Week6/week6/Clickable.kt
@@ -1,6 +1,7 @@
 package oop_06_Arda.Week6.week6
 
-interface Clickable {
-    val name: String // Abstract property, tidak ada nilai default
-    fun click()
+class Button(override val name: String) : Clickable {
+    override fun click() {
+        println("Tombol '$name' berhasil diklik!")
+    }
 }
