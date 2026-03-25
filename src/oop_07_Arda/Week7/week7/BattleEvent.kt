95d66c560194709bf97072df150fa1879cadb5e5
LightVain12 <erdikaarda@gmail.com>
2026-03-25 11:51:31 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_07_Arda/Week7/week7/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_07_Arda.Week7.week7
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}
