7ee38f24ff72b3af9e491627da91b1b9645ddd5c
LightVain12 <erdikaarda@gmail.com>
2026-03-25 11:33:05 +0700

    week07: create ApiResponse sealed class

--- src/oop_07_Arda/Week7/week7/SystemStates.kt
+++ src/oop_07_Arda/Week7/week7/SystemStates.kt
@@ -2,4 +2,11 @@ package oop_07_Arda.Week7.week7
 
 enum class AppState {
     STARTING, RUNNING, STOPPED
+
+    sealed class ApiResponse {
+        data class Success(val data: String) : ApiResponse()
+        data class Error(val message: String) : ApiResponse()
+        object Loading : ApiResponse()
+    }
+
 }
