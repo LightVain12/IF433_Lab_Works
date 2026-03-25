215fd84c9312c41b007f172a1a39bbb185f7b6d2
LightVain12 <erdikaarda@gmail.com>
2026-03-25 11:50:18 +0700

    week07: (task) create GameItem data class

--- src/oop_07_Arda/Week7/week7/GameModels.kt
+++ src/oop_07_Arda/Week7/week7/GameModels.kt
@@ -3,3 +3,5 @@ package oop_07_Arda.Week7.week7
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(20), RARE(8), EPIC(1), LEGENDARY(1)
 }
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity)
