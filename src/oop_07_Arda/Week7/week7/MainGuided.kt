724a5fce6b94ccd7cc3b9de791692c5584e590fd
LightVain12 <erdikaarda@gmail.com>
2026-03-25 11:34:33 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_07_Arda/Week7/week7/MainGuided.kt
+++ src/oop_07_Arda/Week7/week7/MainGuided.kt
@@ -29,11 +29,11 @@ fun main() {
 
     println("\n=== TEST SEALED CLASS ===")
     val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-
-    // ERROR: 'when' expression must be exhaustive (karena Loading tidak dimasukkan)
-    // val uiMessage = when (response) {
-    //     is ApiResponse.Success -> "Tampilkan: ${response.data}"
-    //     is ApiResponse.Error -> "Munculkan alert: ${response.message}"
-    // }
+    val uiMessage = when (response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Tampilkan Spinner"
+    }
+    println(uiMessage)
 
 }
