01898751434c0c977aebfde316c2ef4161e48be9
LightVain12 <erdikaarda@gmail.com>
2026-03-25 11:38:18 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_07_Arda/Week7/week7/SystemStates.kt
+++ src/oop_07_Arda/Week7/week7/SystemStates.kt
@@ -1,12 +1,13 @@
 package oop_07_Arda.Week7.week7
 
+// CHECKPOINT 8
 enum class AppState {
     STARTING, RUNNING, STOPPED
+}
 
-    sealed class ApiResponse {
-        data class Success(val data: String) : ApiResponse()
-        data class Error(val message: String) : ApiResponse()
-        object Loading : ApiResponse()
-    }
-
+// CHECKPOINT 9
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
 }
