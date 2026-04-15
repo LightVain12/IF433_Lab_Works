df5d06ce76440c4ae3d95e043c392e2f756bd4f5
LightVain12 <erdikaarda@gmail.com>
2026-04-15 09:02:01 +0700

    week08: create nested classes for Order

---
+++ src/oop_08_Arda/Week8/week8/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_08_Arda.Week8.week8
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
