5beb0a084f9e5ded8708bdfc273f5f8ef86078ad
LightVain12 <erdikaarda@gmail.com>
2026-04-15 09:02:45 +0700

    week08: implement chained safe calls and elvis

---
+++ src/oop_08_Arda/Week8/week8/MainGuided.kt
@@ -0,0 +1,10 @@
+package oop_08_Arda.Week8.week8
+
+fun main() {
+    // --- CHECKPOINT 4 ---
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+}
