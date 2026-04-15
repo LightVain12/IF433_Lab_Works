18bae63112a795434951e4187d02724dae3b30ae
LightVain12 <erdikaarda@gmail.com>
2026-04-15 10:56:53 +0700

    week08: (task) implement payment processing with !!

--- src/oop_08_Arda/Week8/week8/ApiParser.kt
+++ src/oop_08_Arda/Week8/week8/ApiParser.kt
@@ -1,12 +1,8 @@
 package oop_08_Arda.Week8.week8
 
 class ApiParser {
-}package oop_08_Arda.Week8.week8
-
-class ApiParser {
 
     fun parseProduct(rawJson: Map<String, Any?>): Product? {
-        // Menggunakan requireNotNull untuk memastikan id dan name WAJIB ada
         val id = requireNotNull(rawJson["id"] as? String) { "API Invalid: Missing ID" }
         val name = requireNotNull(rawJson["name"] as? String) { "API Invalid: Missing Name" }
 
@@ -14,16 +10,26 @@ class ApiParser {
 
         return when (type) {
             "ELECTRONIC" -> {
-                // Fallback dengan Elvis operator jika warranty tidak valid/kosong
                 val warranty = rawJson["warranty"] as? Int ?: 12
                 Electronic(id, name, warranty)
             }
             "CLOTHING" -> {
-                // Fallback dengan Elvis operator jika size kosong
                 val size = rawJson["size"] as? String ?: "All Size"
                 Clothing(id, name, size)
             }
-            else -> null // Kembalikan null jika tipenya tidak dikenal
+            else -> null
         }
+    }
+
+    fun checkout(product: Product) {
+        // Smart Cast berdasarkan tipe Product
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+
+        // Memanggil fungsi dari Java. Kita gunakan !! karena kita yakin sistem tidak akan mengembalikan null
+        val result = JavaPaymentService.processPayment(id)
+        println("Berhasil Checkout: ${result!!}")
     }
 }
