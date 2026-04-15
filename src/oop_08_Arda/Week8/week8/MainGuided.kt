1e44f6bd19e8927aa695a0bfd37f5311873f6cad
LightVain12 <erdikaarda@gmail.com>
2026-04-15 09:24:59 +0700

    week08: implement java interop using !!

--- src/oop_08_Arda/Week8/week8/MainGuided.kt
+++ src/oop_08_Arda/Week8/week8/MainGuided.kt
@@ -42,5 +42,10 @@ fun main() {
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    // Karena dari Java, Kotlin menganggapnya String!, kita paksa pakai !!
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 
 }
