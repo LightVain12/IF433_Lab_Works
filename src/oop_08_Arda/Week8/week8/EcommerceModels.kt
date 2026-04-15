951905e064ea6b4693704f32f0c00e25b498b8a0
LightVain12 <erdikaarda@gmail.com>
2026-04-15 10:55:13 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_08_Arda/Week8/week8/EcommerceModels.kt
@@ -0,0 +1,7 @@
+package oop_08_Arda.Week8.week8
+
+sealed class Product
+
+data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+
+data class Clothing(val id: String, val name: String, val size: String) : Product()
