8d9f73c8b16ff26cb8cfc9850a4964c397bd0308
LightVain12 <erdikaarda@gmail.com>
2026-04-15 10:59:52 +0700

    week08: (task) test full e-commerce pipeline in main

--- src/oop_08_Arda/Week8/week8/LegacyJavaAPI.java
+++ src/oop_08_Arda/Week8/week8/LegacyJavaAPI.java
@@ -1,7 +1,6 @@
 package oop_08_Arda.Week8.week8;
 
 public class LegacyJavaAPI {
-    // Fungsi Java tidak punya Null Safety bawaan
     public static String fetchServerStatus() {
         return "SERVER_ONLINE_200_OK";
     }
