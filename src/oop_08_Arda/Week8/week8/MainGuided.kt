b5cb9c7f24f70bb15223e5d73521dabb25afa28a
LightVain12 <erdikaarda@gmail.com>
2026-04-15 09:21:15 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_08_Arda/Week8/week8/MainGuided.kt
+++ src/oop_08_Arda/Week8/week8/MainGuided.kt
@@ -22,4 +22,11 @@ fun main() {
     val mixedData: List<Any> = listOf(
         "Smartphone", 1500000, UserProfile("Andi", null), "Laptop", 4500000.0
     )
+    // --- CHECKPOINT 7 ---
+    for (item in mixedData) {
+        val text = item as? String // Hanya mengambil yang bertipe String
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
