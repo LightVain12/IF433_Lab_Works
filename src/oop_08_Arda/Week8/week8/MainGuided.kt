c8ac737856025f18347bde86c87af280fc56e8f4
LightVain12 <erdikaarda@gmail.com>
2026-04-15 09:22:42 +0700

    week08: (trial) trigger NullPointerException with !!

--- src/oop_08_Arda/Week8/week8/MainGuided.kt
+++ src/oop_08_Arda/Week8/week8/MainGuided.kt
@@ -1,13 +1,11 @@
 package oop_08_Arda.Week8.week8
 
 fun main() {
-    // --- CHECKPOINT 4 ---
     println("=== TEST SAFE CALLS & ELVIS ===")
     val emptyOrder = Order(null, null)
 
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
-    // --- CHECKPOINT 5 ---
     println("\n=== TEST LET BLOCK ===")
     val validOrder = Order(null, 250000)
 
@@ -17,22 +15,26 @@ fun main() {
     } ?: "Transaksi Invalid: Harga belum di-set!"
 
     println(receipt)
-    // --- CHECKPOINT 6 ---
     println("\n=== TEST SAFE CASTING ===")
     val mixedData: List<Any> = listOf(
         "Smartphone", 1500000, UserProfile("Andi", null), "Laptop", 4500000.0
     )
-    // --- CHECKPOINT 7 ---
     for (item in mixedData) {
         val text = item as? String // Hanya mengambil yang bertipe String
         text?.let {
             println("Ditemukan teks: ${it.uppercase()}")
         }
     }
-
-    // --- CHECKPOINT 8 ---
     val someObject: Any = 100
     val safeString = someObject as? String ?: "Unknown String"
     println("Hasil cast fallback: $safeString")
+
+    println("\n=== TEST THE RED BUTTON (!!) ===")
+    val toxicData: String? = null
+    try {
+        val length = toxicData!!.length
+    } catch (e: NullPointerException) {
+        println("CRASH (NPE)! Jangan gunakan !! secara sembarangan.")
+    }
 
 }
