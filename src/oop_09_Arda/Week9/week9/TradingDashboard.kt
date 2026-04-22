9c9d5d97b8b4c765540765f095cd32198d28ead6
LightVain12 <erdikaarda@gmail.com>
2026-04-22 17:39:35 +0700

    week09: (task) extract unique trading pairs using map and toSet

--- src/oop_09_Arda/Week9/week9/TradingDashboard.kt
+++ src/oop_09_Arda/Week9/week9/TradingDashboard.kt
@@ -24,5 +24,5 @@ fun main() {
         .sortedBy { it.roe } // Diurutkan Ascending (dari minus terbesar ke terkecil)
         .map { "LOSS [${it.pair} ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
 
-
+    val uniquePairs = tradeHistory.map { it.pair }.toSet()
 }
