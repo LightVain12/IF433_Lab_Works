1a345c4d4da270f996cfbe743f25cd2c32bf7695
LightVain12 <erdikaarda@gmail.com>
2026-04-29 14:21:10 +0700

    week10: (task) define generic Repository interface

--- src/oop_10_Arda/Week10/week10/CryptoGeneric.kt
+++ src/oop_10_Arda/Week10/week10/CryptoGeneric.kt
@@ -1,3 +1,8 @@
 package oop_10_Arda.Week10.week10
 
 class ApiResponse<T>(val status: Int, val message: String, val data: T)
+
+interface Repository<T> {
+    fun add(item: T)
+    fun getAll(): List<T>
+}
