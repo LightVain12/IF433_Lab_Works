1717878ed3cdd6ad8dd7af7e9e12b81dc113a1d7
LightVain12 <erdikaarda@gmail.com>
2026-04-29 14:18:36 +0700

    week10: (task) create Coin and Transaction data models

--- src/oop_10_Arda/Week10/week10/CryptoModels.kt
+++ src/oop_10_Arda/Week10/week10/CryptoModels.kt
@@ -1,4 +1,5 @@
 package oop_10_Arda.Week10.week10
 
-class CryptoModels {
-}
+data class Coin(val name: String, val symbol: String, val price: Double)
+
+data class Transaction(val txId: String, val amount: Double, val type: String)
