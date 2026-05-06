f9680018b10de24f55abf5d658513c308509a40a
LightVain12 <erdikaarda@gmail.com>
2026-05-06 14:17:01 +0700

    week11: implement let function for null safety

---
+++ src/oop_11_Arda/Week11/week11/ScopeLetRun.kt
@@ -0,0 +1,14 @@
+package oop_11_Arda.Week11.week11
+
+fun main() {
+    // --- CHECKPOINT 4 ---
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    }
+    println("Panjang nama: $length")
+
+}
