598ba0862dd2d167ced625a46277e4328a768ba7
LightVain12 <erdikaarda@gmail.com>
2026-05-06 15:05:02 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_11_Arda/Week11/week11/SmartHomeDashboard.kt
+++ src/oop_11_Arda/Week11/week11/SmartHomeDashboard.kt
@@ -5,4 +5,10 @@ fun main() {
 
     val homeDevices = mutableListOf<SmartDevice>()
 
+    SmartDevice(name = "Philips WiZ Living Room", category = "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
