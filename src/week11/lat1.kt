f2364663db251271a5612b82166e2fca8e987232
LightVain12 <erdikaarda@gmail.com>
2026-05-06 14:15:43 +0700

    week11: create nullable receiver extension

--- src/week11/lat1.kt
+++ src/week11/lat1.kt
@@ -1,4 +1,101 @@
 package week11
 
-class lat1 {
+fun panggil_nama(nama:String):String {
+    return "Hai $nama"
+}
+
+fun String.panggil_mahasiswa():String {
+    var hasil = this.split(" ").joinToString(" ") {
+        it.replaceFirstChar {
+                c -> c.uppercase()
+        }
+    }
+    return hasil
+}
+
+//function Extension - Parameter
+fun Int.tentukanLulus(nama:String):String {
+    var keputusan = ""
+    if (this >= 70) {
+        keputusan = "Lulus"
+    } else {
+        keputusan = "Remedial"    }
+    return "Mahasiswa $nama hasil kelulusan adalah $keputusan"
+}
+
+//function Extension - cek NULL
+fun String?.cekNulldanEmpty():String {
+    if(this == null || this.isEmpty()) {
+        return "gak boleh null atau kosong bang"
+    } else {
+        return "username kamu adalah $this"
+    }
+}
+
+class Mahasiswa() {
+    var nim:String = ""
+    var nama:String = ""
+    var ipk:Int = 0
+}
+
+fun main() {
+    //cara panggil function Extension - NULL
+    var inputUsername:String? = "Wirawan"
+    println(inputUsername.cekNulldanEmpty())
+
+    //cara panggil function Extension - Parameter
+    println(80.tentukanLulus("Revan"))
+
+    //Cara panggil function biasa
+    println(panggil_nama("Toni").uppercase())
+
+    //cara panggil function Extension - BASIC
+    println("tonyyy stark iron man".panggil_mahasiswa())
+
+    //SCOPE - LET
+    var kampusSaya = "UMN"
+    kampusSaya.let {
+        if(it == "UMN") {
+            println("kampus " + it + " Keren")
+        } else {
+            println("Amazing $it")
+        }
+    }
+
+    //SCOPE - RUN
+    var alamatSaya = "Gading Serpong"
+    alamatSaya.run {
+        println(this)
+    }
+
+    //SCOPE - WITH
+    var gradeSaya = with(90){
+        if(this >= 80) {
+            "A"
+        } else if (this >= 70) {
+            "B"
+        } else {
+            "C"
+        }
+    }
+    println("Grade kamu adalah $gradeSaya")
+
+    //SCOPE - APPLY
+    var dataMahasiswa = Mahasiswa().apply {
+        nim = "12345"
+        nama = "Yudi"
+        ipk = 3
+    }
+    println("Mahasiswa ${dataMahasiswa.nama} dgn nim ${dataMahasiswa.nim} ipk nya ${dataMahasiswa.ipk}")
+
+    //SCOPE - ALSO
+    var deretAngka = mutableListOf<Int>(80,70,75,68,85)
+    deretAngka.also {
+        println("Sebelum $deretAngka")
+    }.add(90)
+    println("Setelah $deretAngka")
+
+    //unused
+    //var coba = "Udin"
+    //println(coba.repeat(2))
 }
