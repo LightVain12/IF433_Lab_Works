66e3e9ec25c9811b971f7746eb98ecc9f5bf4c93
LightVain12 <erdikaarda@gmail.com>
2026-05-06 15:02:10 +0700

    week11: implement with function for multiple operations

--- src/oop_11_Arda/Week11/week11/ScopeConfiguration.kt
+++ src/oop_11_Arda/Week11/week11/ScopeConfiguration.kt
@@ -2,7 +2,6 @@ package oop_11_Arda.Week11.week11
 
 data class User(var name: String = "", var age: Int = 0)
 fun main() {
-    // --- CHECKPOINT 7: apply ---
     println("=== TEST APPLY ===")
     val user = User().apply {
         name = "Alex"
