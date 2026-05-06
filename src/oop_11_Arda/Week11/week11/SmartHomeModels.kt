ffdeccb6ff3363af561ae66e0a42bf178d8463aa
LightVain12 <erdikaarda@gmail.com>
2026-05-06 15:03:38 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_11_Arda/Week11/week11/SmartHomeModels.kt
+++ src/oop_11_Arda/Week11/week11/SmartHomeModels.kt
@@ -6,3 +6,8 @@ data class SmartDevice(
     var isOnline: Boolean = false,
     var powerLoad: Int = 0
 )
+
+fun SmartDevice.diagnose(): String {
+    val statusStr = if (isOnline) "Online" else "Offline"
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: $statusStr | Daya: ${powerLoad}W"
+}
