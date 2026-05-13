83ed4919688174a440337cd5b9a05bf520292e48
LightVain12 <erdikaarda@gmail.com>
2026-05-13 22:30:38 +0700

    week12: create basic try-catch-finally

--- src/week12/lat1.kt
+++ src/week12/lat1.kt
@@ -23,9 +23,85 @@ fun cek_tipe_data() {
     println("Angka kamu adalah ${angka}");
 }
 
+//Throw
+fun cek_saldo(saldoSaya: Int) {
+    if(saldoSaya<1000) {
+        throw IllegalArgumentException("Saldo ${saldoSaya} tidak perlu bayar pajak")
+    } else {
+        println("Selamat kamu orang kaya");
+    }
+}
+
+//custom Exception
+class CekNilaiKKM(val kkm:Int, val nilai:Int): Exception("Nilai $nilai berada dibawah kkm $kkm")
+
+class nilai_siswa(val kkm:Int) {
+    fun input_nilai(nilaiKamu:Int) {
+        if(nilaiKamu < kkm) {
+            throw CekNilaiKKM(kkm, nilaiKamu)
+        } else {
+            println("Kamu lulus, dengan nilai $nilaiKamu")
+        }
+    }
+}
+
+//multiple catch
+fun contoh_multiple_catch(nilai:String) {
+    try {
+        val angka = Integer.parseInt(nilai);
+        val hasil = angka/2
+        println("Hasil nilainya: $hasil")
+        cek_saldo(100)
+    } catch (e: NumberFormatException) {
+        println("Error salah format ${e.message}")
+    } catch (e: ArithmeticException) {
+        println("Error kesalahan Aritmatika ${e.message}")
+    } catch (e: Exception) {
+        println("Error program :  ${e.message}")
+    }
+}
+
+sealed class ATMException(pesan: String): Exception(pesan)
+
+class saldo_dibawak_nol(val saldoKamu:Int):
+    ATMException("Masa saldo $saldoKamu minus")
+class cek_jumlah_saldo(val saldoAwal:Int, val jajan:Int):
+    ATMException("Saldo tinggal $saldoAwal, kamu jajan $jajan, ga cukup bang")
+
+fun mulai_jajan(saldoAwalKamu:Int, jajanKamu:Int):Int {
+    if(jajanKamu > saldoAwalKamu) {
+        throw cek_jumlah_saldo(saldoAwalKamu, jajanKamu)
+    } else if (saldoAwalKamu<0) {
+        throw saldo_dibawak_nol(saldoAwalKamu)
+    } else {
+        println("Transaksi berhasil!!!!, saldo sisa: ")
+    }
+    return saldoAwalKamu - jajanKamu;
+}
 
 fun main() {
+    runCatching { mulai_jajan(1000, 900) }
+        .onSuccess{ println("Transaksi Sukses: $it")}
+        .onFailure{ println("Transaksi Gagal $it")}
+
+    contoh_multiple_catch("50")
+
+    //panggil class custom Exception
+    val nsiswa = nilai_siswa(70);
+    try {
+        nsiswa.input_nilai(71);
+    } catch (e:Exception) {
+        println("Ada error saat input nilai ${e.message}");
+    }
+    //Coba Throw
+    try {
+        cek_saldo(1200)
+    } catch (e: Exception) {
+        println("Error diThrow: ${e.message}")
+    }
     //Exception - Expression
     cek_tipe_data()
+
+    //Exception basic
     pembagian(10,0)
 }
