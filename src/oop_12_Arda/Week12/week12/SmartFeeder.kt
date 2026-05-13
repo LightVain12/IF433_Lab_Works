d17c57b427fee3b70780687b5206dde5a3cd13a1
LightVain12 <erdikaarda@gmail.com>
2026-05-13 22:45:57 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_12_Arda/Week12/week12/SmartFeeder.kt
+++ src/oop_12_Arda/Week12/week12/SmartFeeder.kt
@@ -17,4 +17,16 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    println("=== JADWAL MAKAN 1 (PAGI) ===")
+    try {
+        dispenseKibble(requestedGram = 80, availableGram = currentKibbleStock, isJammed = false)
+    } catch (e: DispenserJamException) {
+        println("Caught Domain Error: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Caught Domain Error: ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: ${e.message}")
+    }
+
 }
